03be36d488aaafc99db91f5c7c609f9f5a476667
Adelline Kania <adellinekaniasetiyawan@gmail.com>
2024-05-11 14:18:48 +0700

    filter show value

--- src/components/detailstats/h2hwinstats.tsx
+++ src/components/detailstats/h2hwinstats.tsx
@@ -9,30 +9,56 @@ import {
 
 export function H2HWinStats({ nationcode }: Readonly<{ nationcode: string }>) {
     const [datas, setDatas] = useState(
-        data(nationcode, gcode2Nat, nationdata, "", 10)
+        data(nationcode, gcode2Nat, nationdata, "matches", 10, "all")
     );
 
     const [totalH2h, setTotalH2h] = useState(0);
 
     const [showSortDropdown, setShowSortDropdown] = useState(false);
+    const [showValueDropdown, setShowValueDropdown] = useState(false);
 
-    const [sortByValue, setSortByValue] = useState("");
+    const [sortByValue, setSortByValue] = useState("matches");
+    const [showValue, setShowValue] = useState("all");
     const [totalH2hValue, setTotalH2hValue] = useState(10);
 
     useEffect(() => {
         setShowSortDropdown(false);
+        setShowValueDropdown(false);
         setDatas(
-            data(nationcode, gcode2Nat, nationdata, sortByValue, totalH2hValue)
+            data(
+                nationcode,
+                gcode2Nat,
+                nationdata,
+                sortByValue,
+                totalH2hValue,
+                showValue
+            )
         );
-    }, [sortByValue, totalH2hValue]);
+    }, [sortByValue, totalH2hValue, showValue]);
 
     useEffect(() => {
-        setShowSortDropdown(false);
-        setSortByValue("");
-        setTotalH2hValue(10);
+        resetFilterSort();
+
         updateTotalH2hData(nationcode, gcode2Nat, nationdata);
-        setDatas(data(nationcode, gcode2Nat, nationdata, "", totalH2hValue));
+        setDatas(
+            data(
+                nationcode,
+                gcode2Nat,
+                nationdata,
+                "matches",
+                totalH2hValue,
+                "all"
+            )
+        );
     }, [nationcode]);
+
+    function resetFilterSort() {
+        setSortByValue("matches");
+        setShowValue("all");
+        setShowSortDropdown(false);
+        setShowValueDropdown(false);
+        setTotalH2hValue(10);
+    }
 
     function updateTotalH2hData(
         ncode: string,
@@ -49,14 +75,15 @@ export function H2HWinStats({ nationcode }: Readonly<{ nationcode: string }>) {
         codemap: NationCodeMap,
         datanation: NationsAllStats,
         sortBy: string,
-        limit: number
+        limit: number,
+        show: string
     ) {
         const arrdata = [];
         arrdata.push([
             "Head-to-Head",
-            "Win",
+            "Wins",
             { role: "style" },
-            "Lose",
+            "Losses",
             { role: "style" },
         ]);
         const code = codemap[ncode];
@@ -74,14 +101,14 @@ export function H2HWinStats({ nationcode }: Readonly<{ nationcode: string }>) {
         }
 
         // Descending Sort
-        const descendsort = arrdata.sort((a, b) => {
+        let descendsort = arrdata.sort((a, b) => {
             let numA = a[1] as number;
             let numB = b[1] as number;
 
-            if (sortBy === "match") {
+            if (sortBy === "matches") {
                 numA += a[3] as number;
                 numB += b[3] as number;
-            } else if (sortBy === "lose") {
+            } else if (sortBy === "losses") {
                 numA = a[3] as number;
                 numB = b[3] as number;
             }
@@ -89,10 +116,22 @@ export function H2HWinStats({ nationcode }: Readonly<{ nationcode: string }>) {
         });
 
         if (limit > 0) {
-            return descendsort.slice(0, limit + 1);
+            descendsort = descendsort.slice(0, limit + 1);
+        } else {
+            descendsort = descendsort.slice(0, 11);
         }
 
-        return descendsort.slice(0, 11);
+        if (show === "wins") {
+            descendsort.forEach((item) => {
+                item.splice(3, 4);
+            });
+        } else if (show === "losses") {
+            descendsort.forEach((item) => {
+                item.splice(1, 2);
+            });
+        }
+
+        return descendsort;
     }
 
     const options = {
@@ -107,86 +146,173 @@ export function H2HWinStats({ nationcode }: Readonly<{ nationcode: string }>) {
     };
 
     const sortByOptions = [
-        { value: "match", label: "Number of Match" },
-        { value: "win", label: "Number of Win" },
-        { value: "lose", label: "Number of Lose" },
+        { value: "matches", label: "Number of Matches" },
+        { value: "wins", label: "Number of Wins" },
+        { value: "losses", label: "Number of Losses" },
+    ];
+
+    const showOptions = [
+        { value: "all", label: "All" },
+        { value: "wins", label: "Wins" },
+        { value: "losses", label: "Losses" },
     ];
 
     return (
         <>
-            <div className="flex flex-col gap-4 my-4">
-                <div className="relative" data-twe-dropdown-ref>
-                    <button
-                        className="flex items-center rounded bg-primary pl-4 pr-6 pb-2 pt-2.5 text-sm font-medium leading-normal text-white transition duration-150 ease-in-out border-white focus:outline-none focus:ring-0 hover:border hover:border-red-500 focus:border-red-500"
-                        type="button"
-                        id="dropdownMenuButton1"
-                        data-twe-dropdown-toggle-ref
-                        aria-expanded="false"
-                        data-twe-ripple-init
-                        data-twe-ripple-color="light"
-                        onClick={() => setShowSortDropdown(!showSortDropdown)}
-                    >
-                        {sortByValue === ""
-                            ? "Sort By"
-                            : sortByOptions.find(
-                                  (option) => option.value === sortByValue
-                              )?.label}
-                        <span className="ms-2 w-2 [&>svg]:h-5 [&>svg]:w-5">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
+            <div className="flex flex-col gap-4 my-4 w-fit">
+                <div className="flex items-center gap-4">
+                    <div className="flex flex-col items-start gap-2">
+                        <span className="text-xs">Sort By:</span>
+                        <div className="relative" data-twe-dropdown-ref>
+                            <button
+                                className="flex items-center justify-between rounded bg-primary pl-4 pr-6 pb-2 pt-2.5 w-[200px] text-sm font-medium leading-normal text-white transition duration-150 ease-in-out border-white focus:outline-none focus:ring-0 hover:border hover:border-red-500 focus:border-red-500"
+                                type="button"
+                                id="dropdownMenuButton1"
+                                data-twe-dropdown-toggle-ref
+                                aria-expanded="false"
+                                data-twe-ripple-init
+                                data-twe-ripple-color="light"
+                                onClick={() =>
+                                    setShowSortDropdown(!showSortDropdown)
+                                }
                             >
-                                <path
-                                    fill-rule="evenodd"
-                                    d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                                    clip-rule="evenodd"
-                                />
-                            </svg>
-                        </span>
-                    </button>
-                    {showSortDropdown && (
-                        <ul
-                            className="absolute z-[1000] float-left m-0 min-w-max list-none overflow-hidden rounded-lg border-none bg-white bg-clip-padding text-base shadow-lg data-[twe-dropdown-show]:block dark:bg-surface-dark"
-                            aria-labelledby="dropdownMenuButton1"
-                            data-twe-dropdown-menu-ref
-                        >
-                            {sortByOptions.map((option) => (
-                                <li
-                                    onClick={() => setSortByValue(option.value)}
-                                >
-                                    <a
-                                        className="block w-full whitespace-nowrap bg-white pl-4 pr-8 py-2 text-sm font-normal text-black hover:bg-zinc-200/60 hover:text-black text-left"
-                                        href="#"
-                                        data-twe-dropdown-item-ref
+                                {
+                                    sortByOptions.find(
+                                        (option) => option.value === sortByValue
+                                    )?.label
+                                }
+                                <span className="ms-2 w-2 [&>svg]:h-5 [&>svg]:w-5">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 20 20"
+                                        fill="currentColor"
                                     >
-                                        {option.label}
-                                    </a>
-                                </li>
-                            ))}
-                        </ul>
-                    )}
-                </div>
-                <div className="flex flex-col items-start gap-2">
-                    <div className="flex items-center gap-2">
-                        <span className="text-sm">Show</span>
-                        <input
-                            className="shadow appearance-none border rounded w-[72px] py-2 px-3 text-xs text-white leading-tight focus:outline-none focus:shadow-outline"
-                            id="total-h2h"
-                            type="number"
-                            placeholder="10"
-                            value={totalH2hValue}
-                            onChange={(e) => {
-                                setTotalH2hValue(parseInt(e.target.value));
-                            }}
-                        ></input>
-                        <span className="text-sm">from {totalH2h} data</span>
+                                        <path
+                                            fill-rule="evenodd"
+                                            d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                                            clip-rule="evenodd"
+                                        />
+                                    </svg>
+                                </span>
+                            </button>
+                            {showSortDropdown && (
+                                <ul
+                                    className="absolute z-[1000] float-left m-0 min-w-max list-none overflow-hidden rounded-lg border-none bg-white bg-clip-padding text-base shadow-lg data-[twe-dropdown-show]:block dark:bg-surface-dark"
+                                    aria-labelledby="dropdownMenuButton1"
+                                    data-twe-dropdown-menu-ref
+                                >
+                                    {sortByOptions.map((option) => (
+                                        <li
+                                            onClick={() =>
+                                                setSortByValue(option.value)
+                                            }
+                                        >
+                                            <a
+                                                className="block w-full whitespace-nowrap bg-white pl-4 pr-8 py-2 text-sm font-normal text-black hover:bg-zinc-200/60 hover:text-black text-left"
+                                                href="#"
+                                                data-twe-dropdown-item-ref
+                                            >
+                                                {option.label}
+                                            </a>
+                                        </li>
+                                    ))}
+                                </ul>
+                            )}
+                        </div>
                     </div>
-                    <span className="text-xs text-red-500">
-                        {totalH2hValue > totalH2h
-                            ? `Total maximum data is ${totalH2h}`
-                            : ""}
-                    </span>
+                    <div className="flex flex-col items-start gap-2">
+                        <span className="text-xs">Show Attributes:</span>
+                        <div className="relative" data-twe-dropdown-ref>
+                            <button
+                                className="flex items-center justify-between rounded bg-primary pl-4 pr-6 pb-2 pt-2.5 w-[106px] text-sm font-medium leading-normal text-white transition duration-150 ease-in-out border-white focus:outline-none focus:ring-0 hover:border hover:border-red-500 focus:border-red-500"
+                                type="button"
+                                id="dropdownMenuButton1"
+                                data-twe-dropdown-toggle-ref
+                                aria-expanded="false"
+                                data-twe-ripple-init
+                                data-twe-ripple-color="light"
+                                onClick={() =>
+                                    setShowValueDropdown(!showValueDropdown)
+                                }
+                            >
+                                {
+                                    showOptions.find(
+                                        (option) => option.value === showValue
+                                    )?.label
+                                }
+                                <span className="ms-2 w-2 [&>svg]:h-5 [&>svg]:w-5">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 20 20"
+                                        fill="currentColor"
+                                    >
+                                        <path
+                                            fill-rule="evenodd"
+                                            d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                                            clip-rule="evenodd"
+                                        />
+                                    </svg>
+                                </span>
+                            </button>
+                            {showValueDropdown && (
+                                <ul
+                                    className="absolute z-[1000] float-left m-0 min-w-max list-none overflow-hidden rounded-lg border-none bg-white bg-clip-padding text-base shadow-lg data-[twe-dropdown-show]:block dark:bg-surface-dark"
+                                    aria-labelledby="dropdownMenuButton1"
+                                    data-twe-dropdown-menu-ref
+                                >
+                                    {showOptions.map((option) => (
+                                        <li
+                                            onClick={() =>
+                                                setShowValue(option.value)
+                                            }
+                                        >
+                                            <a
+                                                className="block w-full whitespace-nowrap bg-white pl-4 pr-8 py-2 text-sm font-normal text-black hover:bg-zinc-200/60 hover:text-black text-left"
+                                                href="#"
+                                                data-twe-dropdown-item-ref
+                                            >
+                                                {option.label}
+                                            </a>
+                                        </li>
+                                    ))}
+                                </ul>
+                            )}
+                        </div>
+                    </div>
+                </div>
+                <div className="flex items-center justify-between gap-4">
+                    <div className="flex flex-col items-start gap-2">
+                        <div className="flex items-center gap-2">
+                            <span className="text-sm">Show</span>
+                            <input
+                                className="shadow appearance-none border rounded w-[72px] py-2 px-3 text-xs text-white leading-tight focus:outline-none focus:shadow-outline"
+                                id="total-h2h"
+                                type="number"
+                                placeholder="10"
+                                value={totalH2hValue}
+                                onChange={(e) => {
+                                    setTotalH2hValue(parseInt(e.target.value));
+                                }}
+                            ></input>
+                            <span className="text-sm">
+                                from {totalH2h} data
+                            </span>
+                        </div>
+                        <span className="text-xs text-red-500">
+                            {totalH2hValue > totalH2h
+                                ? `Total maximum data to show is ${totalH2h}`
+                                : ""}
+                            {totalH2hValue < 1
+                                ? "Total minimum data to show is 0"
+                                : ""}
+                        </span>
+                    </div>
+                    <div
+                        className="text-sm underline cursor-pointer pb-2"
+                        onClick={resetFilterSort}
+                    >
+                        Reset
+                    </div>
                 </div>
             </div>
             <Chart
